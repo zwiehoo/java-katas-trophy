88b771a13683756d2b7bbcb7c881495a4ffe82b7
Tomasz Zwierzchowski <tomasz.zwierzchowski@allegro.pl>
2016-03-22 12:49:17 +0100

    Missing gradle and settings

--- src/test/java/pl/zwiehoo/katastrophy/kata01/PhraseSplitterTest.java
+++ src/test/java/pl/zwiehoo/katastrophy/kata01/PhraseSplitterTest.java
@@ -1,8 +1,8 @@
 package pl.zwiehoo.katastrophy.kata01;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -15,10 +15,12 @@ public class PhraseSplitterTest {
 
     private static final String SINGLE_WORD = "word";
     private static final String COMPLEX_PHRASE = "The best preparation for tomorrow is doing your best today";
+    private static final int MAX_WORDS_IN_PHRASE = 3;
+    private static final String PHRASE_OF_UNKNOWN_WORDS = "all unknown words";
 
     private PhraseSplitter createPhraseSplitter(Set<String> knownPhrases) {
-        // Replace with your own instance of PhraseSplitter
-        return Mockito.mock(PhraseSplitter.class);
+        SetPhraseDictionary dictionary = new SetPhraseDictionary(knownPhrases);
+        return new DictionaryPhraseSplitter(dictionary, MAX_WORDS_IN_PHRASE);
     }
 
     @Test
@@ -36,7 +38,7 @@ public class PhraseSplitterTest {
     @Test
     public void shouldReturnPhraseCorrectlySplitByGivenComplexPhrase() {
         // Given
-        HashSet<String> knownPhrases = Sets.newHashSet("the best", "is doing");
+        HashSet<String> knownPhrases = Sets.newHashSet("the best", "is doing", "your best today");
         PhraseSplitter phraseSplitter = createPhraseSplitter(knownPhrases);
 
         // When
@@ -44,7 +46,34 @@ public class PhraseSplitterTest {
 
         // Then
         assertEquals(
-                Sets.newHashSet("The best", "preparation", "for", "tomorrow", "is doing", "your", "best", "today"),
+                Lists.newArrayList("The best", "preparation", "for", "tomorrow", "is doing", "your best today"),
+                splitResult
+        );
+    }
+
+    @Test
+    public void shouldReturnEmptyListForEmptyPhrase() {
+        // Given
+        PhraseSplitter phraseSplitter = createPhraseSplitter(Sets.newHashSet());
+
+        // When
+        List<String> splitResult = phraseSplitter.split("");
+
+        // Then
+        assertEquals(Collections.emptyList(), splitResult);
+    }
+
+    @Test
+    public void shouldReturnListOfSingleWordsForAllUnknownPhrases() {
+        // Given
+        PhraseSplitter phraseSplitter = createPhraseSplitter(Sets.newHashSet());
+
+        // When
+        List<String> splitResult = phraseSplitter.split(PHRASE_OF_UNKNOWN_WORDS);
+
+        // Then
+        assertEquals(
+                Lists.newArrayList("all", "unknown", "words"),
                 splitResult
         );
     }
